49d10eec21791736d34cc4906c84bb30b332865a
ryo itakura <ryoi8888@gmail.com>
2024-12-15 14:10:36 +0900

    コミット

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -6,6 +6,7 @@ sudo mkdir -p ./.amplify-hosting/compute/default
 
 sudo cp -r ./build ./.amplify-hosting/compute/default/build
 sudo cp -r server.js ./.amplify-hosting/compute/default
+sudo cp -r bin/package.json ./.amplify-hosting/compute/default
 sudo cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
 sudo cp -r public ./.amplify-hosting/static
