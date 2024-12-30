88b045578b454adb349f1dde2ee25d259fe6e690
ryo itakura <ryoi8888@gmail.com>
2024-12-30 15:06:16 +0900

    no message

--- app/routes/thread.$thread.tsx
+++ app/routes/thread.$thread.tsx
@@ -2,5 +2,6 @@ import { useParams } from "@remix-run/react";
 
 export default function ThreadPage() {
     const params = useParams();
+    console.log(params);
     return <h1>Threasd Page {params.threadid}</h1>;
-  }
+}
